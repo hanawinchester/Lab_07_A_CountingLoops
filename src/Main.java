e79a76740f8f47e47d12c53afcc477cf31e47748
hanawinchester <hana.rose.winchester@gmail.com>
2022-07-03 11:19:03 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for (int cnt=0; cnt<=30; cnt++)
+            System.out.println(cnt);
+
+        System.out.println("\r\n ");
+
+        for (int cnt=30; cnt>=0; cnt--)
+            System.out.println(cnt);
+
+        System.out.println("\r\n ");
+
+        for (int cnt=0; cnt<=18; cnt=cnt+3)
+            System.out.println(cnt);
+
+        System.out.println("\r\n ");
+
+        for (int cnt=10; cnt>=0; cnt=cnt-2)
+            System.out.println(cnt);
+    }
+}
